2723ded68fd1dcf627717f2880f0c9f764a41d47
unknown <18719091650@163.com>
2018-07-22 16:37:57 +0800

    modify

--- novel/updateNotice.py
+++ novel/updateNotice.py
@@ -16,9 +16,15 @@ class UpdateNotice:
         self.init_database()
         self.init_novel()
         self.lock = threading.Lock()
+
         self.parseMethod = {
             "笔趣阁" : self.default_parse,
             "顶点中文网" : self.default_parse,
+        }
+
+        self.parseContentMethod = {
+            "笔趣阁": self.default_parse_content,
+            "顶点中文网": self.default_parse_content,
         }
 
     def init_email(self):
@@ -83,16 +89,16 @@ class UpdateNotice:
         res, dbout = self.mydb.execute(sql)
         return res
 
-    def get_all_send_chapter(self, table):
+    def get_all_chapter(self, table):
         sql = "select name from {}".format(table)
         res, dbout = self.mydb.execute(sql)
         allChapter = [chapter.get("name") for chapter in dbout]
         return allChapter
 
-    def send_email(self, receiver, subject, content):
+    def send_email(self, receiver, subject, content, mimeType="plain"):
         with self.lock:
             self.myemail.set_receiver(receiver)
-            stderr = self.myemail.send_email(subject, content)
+            stderr = self.myemail.send_email(subject, content, mimeType=mimeType)
         return stderr
 
     def parse_novel_thread(self, novel):
@@ -100,25 +106,22 @@ class UpdateNotice:
         table = novel.get("table")
         siteList = novel.get("site")
         receiver = novel.get("receiver")
+        sendContent = novel.get("send_content")
 
         try:
             for site in siteList:
                 if not site.get("status"): continue
                 siteName = site.get("name")
                 parseFunc = self.parseMethod.get(siteName, self.default_parse)
-                newChapter = parseFunc(site)
-                if newChapter and self.save_chapter(siteName, newChapter, table):
-                    stderr = self.send_email(receiver, "小说更新提醒({}-{})".format(novelName, siteName), "最新章节:{}".format(newChapter))
-                    if not stderr:
-                        logger.info("小说更新提醒({0}-{1}-{2}), send email to {3} success!)".format(
-                            novelName, siteName, newChapter, str(receiver)))
+                chapters = parseFunc(site)
+                if chapters and not self.chapter_is_exist(chapters[-1][1], table):
+                    if sendContent:
+                        self.send_chapter_has_content(table, chapters, novelName, siteName, receiver)
                     else:
-                        logger.info("小说更新提醒({0}-{1}-{2}) send email to {3} failed!, error:{4})".format(
-                            novelName, siteName, newChapter, str(receiver), stderr))
-
+                        self.send_chapter(table, chapters[-1][1], novelName, siteName, receiver)
                     break
                 else:
-                    logger.info("{0}({1})最新章节: {2}".format(novelName, siteName, newChapter))
+                    logger.info("{0}({1})最新章节: {2}".format(novelName, siteName, chapters[-1][1]))
         except Exception as e:
             logger.error("parse_novel_thread({0}) error:{1}".format(novelName, str(e)))
 
@@ -130,15 +133,17 @@ class UpdateNotice:
             thread.start()
         [thread.join() for thread in threads]
 
+    def chapter_is_exist(self, chapter, table):
+        chapter = chapter.split(" ")[1].strip()
+        allChapters = self.get_all_chapter(table)
+        return (chapter in allChapters)
+
     def save_chapter(self, siteName, newChapter, table):
         try:
-            chapter = newChapter.split(" ")[1].strip()
-            allChapters = self.get_all_send_chapter(table)
-            if chapter not in allChapters:
-                sql = "insert into {table}(source_site, name, send_time) values('{source_site}', '{name}', now())".format(
-                    table=table, source_site=siteName, name=chapter)
-                self.mydb.execute(sql)
-                return True
+            sql = "insert into {table}(source_site, name, send_time) values('{source_site}', '{name}', now())".format(
+                table=table, source_site=siteName, name=newChapter)
+            self.mydb.execute(sql)
+            return True
         except Exception as e:
             logger.error("save_chapter() error:{}".format(str(e)))
         return False
@@ -157,13 +162,65 @@ class UpdateNotice:
             subNode = soup.body.dl
             for child in subNode.children:
                 try:
-                    chapters.append(child.a.string)
+                    chapterUrl = "{}/{}".format(url, child.a["href"])
+                    charpter = child.a.string
+                    chapters.append((chapterUrl, charpter))
                 except:
                     pass
         except Exception as e:
             logger.error("parse() {site_name} {site_url} error:{errinfo}".format(site_name=name, site_url=url, errinfo=str(e)))
             chapters = None
-        return (None if not chapters else chapters[-1])
+        return chapters
+
+    def default_parse_content(self, url):
+        content = stderr = None
+        try:
+            respon = requests.get(url, headers={
+                'user-agent': 'Mozilla/5.0'
+            }, timeout=10)
+            respon.encoding = "gbk"
+            html = respon.text
+            soup = BeautifulSoup(html, "html.parser")
+            content = str(soup.find(id="content"))
+        except Exception as e:
+            stderr = str(3)
+        return content, stderr
+
+    def send_chapter(self, table, chapter, novelName, siteName, receiver):
+        newChapter = chapter.split(" ")[1].strip()
+        if not self.save_chapter(siteName, newChapter, table): return None
+        subject = "小说更新提醒({0}-{1}-{2})".format(novelName, siteName, chapter)
+        content = "最新章节:{}".format(chapter)
+        stderr = self.send_email(receiver, subject, content)
+        if not stderr:
+            logger.info("{0} send email to {1} success.".format(subject, str(receiver)))
+        else:
+            logger.error("{0} send email to {1} failed!, error:{2}".format(subject, str(receiver), stderr))
+
+    def send_chapter_has_content(self, table, chapters, novelName, siteName, receiver):
+        allChapter = self.get_all_chapter(table)
+        if not self.get_table_count(table): chapters = chapters[-1:]    #如果表中没有数据, 则只发送最新一章
+        for chapterRecord in chapters[::-1]:
+            chapter = chapterRecord[1].split(" ")[1].strip()
+            if (chapter in allChapter) or (not self.save_chapter(siteName, chapter, table)): break
+            url = chapterRecord[0]
+            parseContentFunc = self.parseContentMethod.get(siteName, self.default_parse_content)
+            content, stderr = parseContentFunc(url)
+            if content:
+                content = "<h3>{0}</h3>{1}".format(chapterRecord[1], content)
+                subject = "小说更新提醒({0}-{1}-{2})".format(novelName, siteName, chapterRecord[1])
+                stderr = self.send_email(receiver, subject, content, mimeType="html")
+                if not stderr:
+                    logger.info("{0} send email to {1} success.".format(subject, str(receiver)))
+                else:
+                    logger.error("{0} send email to {1} failed!, error:{2}".format(subject, str(receiver), stderr))
+            else:
+                logger.error("paser url:{0} failed, error:{1}".format(url, stderr))
+
+    def get_table_count(self, table):
+        sql = "select count(*) as charpter_num from {}".format(table)
+        res, dbout = self.mydb.execute(sql)
+        return dbout[0].get("charpter_num")
 
     def run(self):
         while True:
