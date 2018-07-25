e548250b054f41a57358ad397ffbe8b94200b2d3
ouru <18719091650@163.com>
2018-07-25 16:41:24 +0800

    modify

--- novel/main.py
+++ novel/main.py
@@ -12,7 +12,7 @@ def deal_42_signal(signum, stack):
     novelNotice.reset_config("novelConfig.json")
 
 if __name__ == "__main__":
-    # signal.signal(41, deal_41_signal)
-    # signal.signal(42, deal_42_signal)
+    signal.signal(41, deal_41_signal)
+    signal.signal(42, deal_42_signal)
 
     novelNotice.run()
